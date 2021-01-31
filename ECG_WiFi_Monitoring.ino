3c954f93b989e8562553cd9b5c43f230df4d4bc5
Maty Blanc <blancmaty@hotmail.com>
2021-01-31 18:28:11 -0500

    Initial commit

---
+++ ECG_WiFi_Monitoring.ino
@@ -0,0 +1,18 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
+
+void setup()
+{
+  // put your setup code here, to run once:
+
+  for(int i = 0; i < 10; i++)
+  {
+    
+  }
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+
+}
