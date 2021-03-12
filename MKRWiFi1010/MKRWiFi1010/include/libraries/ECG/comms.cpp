10e5c65910af7d11c58fcb02d43a2e80be4da867
Justin.Bee <jbee.appz@gmail.com>
2021-03-11 19:45:48 -0500

    POST implemented

--- MKRWiFi1010/MKRWiFi1010/include/libraries/ECG/comms.cpp
+++ MKRWiFi1010/MKRWiFi1010/include/libraries/ECG/comms.cpp
@@ -11,15 +11,14 @@
 #include "wifi_init.h"
 
 
-const char server[] = "tqmlv0dfbh.execute-api.us-east-1.amazonaws.com/dev/";
-//const char server[] = "master.d30zp0554c2v1e.amplifyapp.com"; //this one works after adding the certificate through Arduino IDE
-
 
 //**************************************************
 //
 //  Local Declarations
 //
 //****************************************************
+const char server[] = "tqmlv0dfbh.execute-api.us-east-1.amazonaws.com";
+
 String JSONBuilder(void);
 
 
@@ -36,64 +35,50 @@ String JSONBuilder(void);
 //*******************************************************************
 void task_WiFiComm(void *pvParamters)
 {
-	//WiFiSSLClient client;
 	WiFiClient client;
 	
-	//String contentType = "application/x-www-form-urlencoded";
-	//String postData = "serial=testSerial&adc=12.1&temp=98.6";
-	/*
-	POST https://tqmlv0dfbh.execute-api.us-east-1.amazonaws.com/dev/
-
-	Content-Type : application/json
-
-	{
-		"serial":"Test",
-		"adc":"45",
-		"temp":"97.8"
-	}
-	*/
-	String postEntity = String("serial=test&adc=1234&temp=9999");
+	String postEntity = String("{\"serial\":\"Default-001\",\"adc\":\"45\",\"temp\":\"97.8\"}");
 	for(;;)
 	{
 		if(IsWifiConnected())
 		{
 			//Close the connection in case it is still open
-			client.stop();
+			//client.stop();
 			Serial.println("Attempting to connect to server....");
-			if (client.connect(server, 443)) {
+			if( client.connectSSL(server,443) ){
 				Serial.println("connected to server");
-				//client.println("GET / HTTP/1.1");
-				//client.print("Host: ");
-				//client.println(server);
-				//client.println("User-Agent: ArduinoWiFi/1.1");
-				//client.println("Connection: close");
-				//client.println();
-				
-				// Make a HTTP request:
-				/*client.println("POST / HTTP/1.1");
-				//serial=testSerial&adc=12.1&temp=98.6
-				//client.println("Host: www.google.com");
-				//client.println("Connection: close");
-				//client.println("POST  HTTP/1.1");
+				// Make a HTTP POST request:
+				client.println("POST /dev HTTP/1.1");
+				// Host
 				client.print("Host: ");
 				client.println(server);
-				client.println("User-Agent: ArduinoWiFi/1.1");
 				client.println("Connection: close");
-				client.println("Content-Type: application/x-www-form-urlencoded;");
-				client.print("Content-Length: ");
-				client.println(postEntity.length());
+				client.println("Content-Type: application/json");
+				client.println("Content-Length: 49");
+				//client.print("Content-Length: ");
+			    //client.println(postEntity.length());
 				client.println();
-				client.println(postEntity); */
+				client.println(postEntity);
+				client.println(); 
+				client.println();
+				
+				/* while(client.available()==0)//wait until data reception
+				 {
+					 
+				 }
+				 while(client.available()) //write all incoming data characters on serial monitor
+				 {
+					 char codeResponse = client.read();
+					 Serial.write(codeResponse);
+				 } */
+				 client.stop();
 			}
 			else{
 				Serial.println("Not connected to server");
 			}
-			//client.post("/post", contentType, postData);
 		}
 		vTaskDelay(5000);
 	}
-	
-	
 }
 
 
